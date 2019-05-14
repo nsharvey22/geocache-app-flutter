7434bf1ab32326e3c82a1c337a678718957261a2
Nicholas Harvey <nsharvey@mavs.coloradomesa.edu>
2019-05-14 00:12:06 -0600

    initial commit

---
+++ lib/main.dart
@@ -0,0 +1,161 @@
+
+import 'package:flutter/material.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: "Parallax",
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        scaffoldBackgroundColor: Color(0xffFFAF1B),
+      ),
+      home: MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  double rateZero = 0;
+  double rateOne = 0;
+  double rateTwo = 0;
+  double rateThree = 0;
+  double rateFour = 0;
+  double rateFive = 0;
+  double rateSix = 0;
+  double rateSeven = 0;
+  double rateEight = 0;
+
+  String asset;
+  double top;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: NotificationListener(
+        onNotification: (v) {
+          if (v is ScrollUpdateNotification) {
+            //only if scroll update notification is triggered
+            print("scrolling");
+            setState(() {
+              rateEight -= v.scrollDelta / 1;
+              rateSeven -= v.scrollDelta / 1.1;
+              rateSix -= v.scrollDelta / 1.2;
+              rateFive -= v.scrollDelta / 1.5;
+              rateFour -= v.scrollDelta / 2.5;
+              rateThree -= v.scrollDelta / 3.5;
+              rateTwo -= v.scrollDelta / 4;
+              rateOne -= v.scrollDelta / 4.5;
+              rateZero -= v.scrollDelta / 5;
+            });
+          }
+        },
+        child: Stack(
+          children: <Widget>[
+            ParallaxWidget(top: rateZero, asset: "parallax0"),
+            ParallaxWidget(top: rateOne, asset: "parallax1"),
+            ParallaxWidget(top: rateTwo, asset: "parallax2"),
+            ParallaxWidget(top: rateThree, asset: "parallax3"),
+            ParallaxWidget(top: rateFour, asset: "parallax4"),
+            ParallaxWidget(top: rateFive, asset: "parallax5"),
+            ParallaxWidget(top: rateSix, asset: "parallax6"),
+            ParallaxWidget(top: rateSeven, asset: "parallax7"),
+            ParallaxWidget(top: rateEight, asset: "parallax8"),
+            ListView(
+              children: <Widget>[
+                Container(
+                  height: 500,
+                  color: Colors.transparent,
+                ),
+                Container(
+                  color: Color(0xff210002),
+                  width: double.infinity,
+                  padding: EdgeInsets.only(top: 70),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    children: <Widget>[
+                     
+                      Text(
+                        "Scavenger",
+                        style: TextStyle(
+                            fontSize: 51,
+                            fontFamily: "MontSerrat-Regular",
+                            letterSpacing: 1.8,
+                            color: Color(0xffffaf00)),
+                      ),
+                      SizedBox(
+                        height: 20,
+                      ),
+                      SizedBox(
+                        width: 190,
+                        child: Divider(
+                          height: 1,
+                          color: Color(0xffffaf00),
+                        ),
+                      ),
+                      SizedBox(
+                        height: 20,
+                      ),
+                      Text(
+                        "Made By",
+                        style: TextStyle(
+                          fontSize: 15,
+                          fontFamily: "Montserrat-Extralight",
+                          letterSpacing: 1.3,
+                          color: Color(0xffffaf00),
+                        ),
+                      ),
+                      Text(
+                        "Nick Harvey",
+                        style: TextStyle(
+                          fontSize: 20,
+                          fontFamily: "Montserrat-Regular",
+                          letterSpacing: 1.8,
+                          color: Color(0xffffaf00),
+                        ),
+                      ),
+                      SizedBox(
+                        height: 50,
+                      )
+                    ],
+                  ),
+                ),
+              ],
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class ParallaxWidget extends StatelessWidget {
+  const ParallaxWidget({
+    Key key,
+    @required this.top,
+    @required this.asset,
+  }) : super(key: key);
+
+  final double top;
+  final String asset;
+
+  @override
+  Widget build(BuildContext context) {
+    return Positioned(
+      left: -150,
+      top: top,
+      child: Container(
+        height: 550,
+        width: 900,
+        child: Image.asset("assets/$asset.png", fit: BoxFit.cover),
+      ),
+    );
+  }
+}
